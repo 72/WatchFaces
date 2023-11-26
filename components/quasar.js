59eb8f2deca3eb994c8919c45477e41183601492
Juan Flores Mena <by72mena@gmail.com>
2023-11-26 15:53:41 -0800

    Add watchface: Quasar

---
+++ components/quasar.js
@@ -0,0 +1,87 @@
+import getTime from './time.js';
+import * as digit from './quasarNumbers.js';
+
+// Setup UI elements
+let app = document.querySelector('#App');
+let container = document.querySelector('#watchFaceContainer');
+let quasarWrapper = document.querySelector('#quasar svg');
+
+let digitOne = document.querySelector('#DigitOne');
+let digitTwo = document.querySelector('#DigitTwo');
+let digitThree = document.querySelector('#DigitThree');
+let digitFour = document.querySelector('#DigitFour');
+
+// DOM frame mgmt
+let windowWidth;
+let windowHeight;
+let baseValue;
+
+// Define time
+let collection = [digit.num0, digit.num1, digit.num2, digit.num3, digit.num4, digit.num5, digit.num6, digit.num7, digit.num8, digit.num9];
+let currentTime;
+
+let composition;
+
+let ticker;
+
+function setTime(time){
+	currentTime = time; // ie. ['06', 53, '02', 'PM']
+
+	// Detect AM/PM and handle hour hand
+	if(currentTime[3] == "PM"){
+		let pmTime = (Number(currentTime[0]) + 12);		
+		if(pmTime == 24){ 
+			pmTime = '00'
+		} else {
+			pmTime = pmTime.toString();
+		}
+		pmTime = pmTime.toString();console.log(pmTime);
+		digitOne.innerHTML = `${collection[pmTime[0]]}`
+		digitTwo.innerHTML = `${collection[pmTime[1]]}`
+	} else{
+		digitOne.innerHTML = `${collection[currentTime[0][0]]}`
+		digitTwo.innerHTML = `${collection[currentTime[0][1]]}`
+	}
+	// Minute hand
+	digitThree.innerHTML = `${collection[currentTime[1][0]]}`
+	digitFour.innerHTML = `${collection[currentTime[1][1]]}`
+
+}
+
+
+function setAspectRatio() {
+	windowWidth = window.innerWidth;
+	windowHeight = window.innerHeight;
+
+	app.style.height = `${windowHeight}px`;
+
+	if(windowWidth >= windowHeight){
+		baseValue = windowHeight;
+	} else {
+		baseValue = windowWidth;
+	}
+	container.style.width = (baseValue * 0.98) + 'px';
+
+}
+
+function adjustMain() {
+	setAspectRatio();
+}
+
+window.addEventListener("resize", adjustMain);
+
+// Set and Update Time 
+function updateTime(){
+	setTime(getTime());
+	ticker = setTimeout(updateTime, 1000);
+}
+
+// Init
+setTime(getTime());
+setAspectRatio();
+updateTime();
+
+container.style.display = 'block';
+setTimeout( ()=>{
+	container.style.opacity = 1;
+}, 1000);
---
+++ components/quasarNumbers.js
@@ -0,0 +1,65 @@
+// Digits SVG for Quasar watchface
+
+export let num0 = `<path d="M3 160L83 21.4359L243 21.4359L323 160L243 298.564L83 298.564L3 160Z" stroke="#506070" stroke-width="4"/>
+<path d="M23 160L93 38.7564L233 38.7564L303 160L233 281.244L93 281.244L23 160Z" stroke="#506070" stroke-width="4"/>
+<path d="M43 160L103 56.077L223 56.077L283 160L223 263.923L103 263.923L43 160Z" stroke="#506070" stroke-width="4"/>
+<path d="M63 160L113 73.3975L213 73.3975L263 160L213 246.603L113 246.603L63 160Z" stroke="#506070" stroke-width="4"/>
+<path d="M83 160L123 90.718L203 90.718L243 160L203 229.282L123 229.282L83 160Z" stroke="#506070" stroke-width="4"/>`;
+
+export let num1 = `<path d="M240 21.4358L320 160L240 298.564" stroke="#506070" stroke-width="4"/>
+<path d="M230 38.7565L300 160L230 281.244" stroke="#506070" stroke-width="4"/>
+<path d="M220 56.0768L280 160L220 263.923" stroke="#506070" stroke-width="4"/>
+<path d="M210 73.3975L260 160L210 246.603" stroke="#506070" stroke-width="4"/>
+<path d="M200 90.7181L240 160L200 229.282" stroke="#506070" stroke-width="4"/>
+<path d="M239 160H320" stroke="#506070" stroke-width="4"/>`;
+
+export let num2 = `<path d="M84 21.4358L244 21.4358L324 160L4 160L84 298.564L244 298.564" stroke="#506070" stroke-width="4"/>
+<path d="M24 160L94 281.244L234 281.244M94 38.7565L234 38.7565L304 160" stroke="#506070" stroke-width="4"/>
+<path d="M44 160L104 263.923L224 263.923M104 56.0768L224 56.0768L284 160" stroke="#506070" stroke-width="4"/>
+<path d="M64 160L114 246.603L214 246.603M114 73.3975L214 73.3975L264 160" stroke="#506070" stroke-width="4"/>
+<path d="M84 160L124 229.282L204 229.282M124 90.7181L204 90.7181L244 160" stroke="#506070" stroke-width="4"/>`;
+
+export let num3 = `<path d="M80 21.4358L240 21.4358L320 160L240 298.564L80 298.564" stroke="#506070" stroke-width="4"/>
+<path d="M90 38.7565L230 38.7565L300 160L230 281.244L90 281.244" stroke="#506070" stroke-width="4"/>
+<path d="M100 56.0768L220 56.0768L280 160L220 263.923L100 263.923" stroke="#506070" stroke-width="4"/>
+<path d="M110 73.3975L210 73.3975L260 160L210 246.603L110 246.603" stroke="#506070" stroke-width="4"/>
+<path d="M120 90.7181L200 90.7181L240 160L200 229.282L120 229.282" stroke="#506070" stroke-width="4"/>
+<line x1="1" y1="160" x2="320" y2="160" stroke="#506070" stroke-width="4"/>`;
+
+export let num4 = `<path d="M84 21.4358L4 160L324 160M244 21.4358L324 160M324 160L244 298.564" stroke="#506070" stroke-width="4"/>
+<path d="M24 160L94 38.7565M234 38.7565L304 160L234 281.244" stroke="#506070" stroke-width="4"/>
+<path d="M44 160L104 56.0768M224 56.0768L284 160L224 263.923" stroke="#506070" stroke-width="4"/>
+<path d="M64 160L114 73.3975M214 73.3975L264 160L214 246.603" stroke="#506070" stroke-width="4"/>
+<path d="M84 160L124 90.7181M204 90.7181L244 160L204 229.282" stroke="#506070" stroke-width="4"/>`;
+
+export let num5 = `<path d="M244 21.4358L84 21.4358L4 160L324 160L244 298.564L84 298.564" stroke="#506070" stroke-width="4"/>
+<path d="M24 160L94 38.7565L234 38.7565M304 160L234 281.244L94 281.244" stroke="#506070" stroke-width="4"/>
+<path d="M44 160L104 56.0768L224 56.0768M284 160L224 263.923L104 263.923" stroke="#506070" stroke-width="4"/>
+<path d="M64 160L114 73.3975L214 73.3975M264 160L214 246.603L114 246.603" stroke="#506070" stroke-width="4"/>
+<path d="M84 160L124 90.7181L204 90.7181M244 160L204 229.282L124 229.282" stroke="#506070" stroke-width="4"/>`;
+
+export let num6 = `<path d="M3 160L83 298.564L243 298.564L323 160L3 160ZM3 160L83 21.4358L243 21.4358" stroke="#506070" stroke-width="4"/>
+<path d="M233 38.7565L93 38.7565L23 160L93 281.244L233 281.244L303 160" stroke="#506070" stroke-width="4"/>
+<path d="M223 56.0768L103 56.0768L43 160L103 263.923L223 263.923L283 160" stroke="#506070" stroke-width="4"/>
+<path d="M213 73.3975L113 73.3975L63 160L113 246.603L213 246.603L263 160" stroke="#506070" stroke-width="4"/>
+<path d="M203 90.7181L123 90.7181L83 160L123 229.282L203 229.282L243 160" stroke="#506070" stroke-width="4"/>`;
+
+export let num7 = `<path d="M80 21.4358L240 21.4358L320 160L240 298.564" stroke="#506070" stroke-width="4"/>
+<path d="M90 38.7565L230 38.7565L300 160L230 281.244" stroke="#506070" stroke-width="4"/>
+<path d="M100 56.0768L220 56.0768L280 160L220 263.923" stroke="#506070" stroke-width="4"/>
+<path d="M110 73.3975L210 73.3975L260 160L210 246.603" stroke="#506070" stroke-width="4"/>
+<path d="M120 90.7181L200 90.7181L240 160L200 229.282" stroke="#506070" stroke-width="4"/>
+<path d="M239 160H320" stroke="#506070" stroke-width="4"/>`;
+
+export let num8 = `<path d="M3 160L83 21.4359L243 21.4359L323 160L243 298.564L83 298.564L3 160Z" stroke="#506070" stroke-width="4"/>
+<path d="M23 160L93 38.7564L233 38.7564L303 160L233 281.244L93 281.244L23 160Z" stroke="#506070" stroke-width="4"/>
+<path d="M43 160L103 56.077L223 56.077L283 160L223 263.923L103 263.923L43 160Z" stroke="#506070" stroke-width="4"/>
+<path d="M63 160L113 73.3975L213 73.3975L263 160L213 246.603L113 246.603L63 160Z" stroke="#506070" stroke-width="4"/>
+<path d="M83 160L123 90.718L203 90.718L243 160L203 229.282L123 229.282L83 160Z" stroke="#506070" stroke-width="4"/>
+<line x1="4" y1="160" x2="323" y2="160" stroke="#506070" stroke-width="4"/>`;
+
+export let num9 = `<path d="M324 160L244 298.564L84 298.564M324 160L244 21.4358L84 21.4358L4 160L324 160Z" stroke="#506070" stroke-width="4"/>
+<path d="M24 160L94 38.7565L234 38.7565L304 160L234 281.244L94 281.244" stroke="#506070" stroke-width="4"/>
+<path d="M44 160L104 56.077L224 56.077L284 160L224 263.923L104 263.923" stroke="#506070" stroke-width="4"/>
+<path d="M64 160L114 73.3975L214 73.3975L264 160L214 246.603L114 246.603" stroke="#506070" stroke-width="4"/>
+<path d="M84 160L124 90.7179L204 90.7179L244 160L204 229.282L124 229.282" stroke="#506070" stroke-width="4"/>`;
