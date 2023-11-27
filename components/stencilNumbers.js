b6bfcbfb61450919d838147bc20bc80f8fb27b3a
Juan Flores Mena <by72mena@gmail.com>
2023-11-26 18:39:24 -0800

    Add watchface: Stencil

---
+++ components/stencilNumbers.js
@@ -0,0 +1,30 @@
+// Digits SVG for Stencil watchface
+
+export let num0 = `<rect x="144" y="132" width="8" height="132" fill="#212224"/>`;
+
+export let num1 = `<rect x="144" width="8" height="396" fill="#212224"/>`;
+
+export let num2 = `<rect y="132" width="148" height="8" fill="#212224"/>
+<rect x="148" y="256" width="148" height="8" fill="#212224"/>`;
+
+export let num3 = `<rect y="132" width="148" height="8" fill="#212224"/>
+<rect y="256" width="148" height="8" fill="#212224"/>`;
+
+export let num4 = `<rect x="144" width="8" height="132" fill="#212224"/>
+<rect x="144" y="256" width="8" height="140" fill="#212224"/>
+<rect y="256" width="148" height="8" fill="#212224"/>`;
+
+export let num5 = `<rect x="148" y="132" width="148" height="8" fill="#212224"/>
+<rect y="256" width="148" height="8" fill="#212224"/>`;
+
+export let num6 = `<rect x="148" y="132" width="148" height="8" fill="#212224"/>
+<rect x="144" y="256" width="8" height="8" fill="#212224"/>`;
+
+export let num7 = `<rect y="132" width="148" height="8" fill="#212224"/>
+<rect x="144" y="132" width="8" height="264" fill="#212224"/>`;
+
+export let num8 = `<rect x="144" y="132" width="8" height="8" fill="#212224"/>
+<rect x="144" y="256" width="8" height="8" fill="#212224"/>`;
+
+export let num9 = `<rect y="256" width="148" height="8" fill="#212224"/>
+<rect x="144" y="132" width="8" height="8" fill="#212224"/>`;
